ce3d27565c9d608d6dea51dbecb2903bf50dfd49
Jason Ozias <jason.ozias@kroger.com>
2021-07-21 15:58:16 -0400

    Moved over day02 and day03 for 2015

--- aoc2-sol/src/year2015/day02.rs
+++ aoc2-sol/src/year2015/day02.rs
@@ -6,9 +6,61 @@
 // option. All files in the project carrying such notice may not be copied,
 // modified, or distributed except according to those terms.
 
-//! Advent of Code - Day 2
-
-use anyhow::{anyhow, Result};
+//! Advent of Code - Day 2 "I Was Told There Would Be No Math"
+//!
+//!
+//! **--- Day 2: I Was Told There Would Be No Math ---**
+//!
+//! **--- Part 1 ---**
+//!
+//! The elves are running low on wrapping paper, and so they need to submit
+//! an order for more. They have a list of the dimensions (length l, width w,
+//! and height h) of each present, and only want to order exactly as much as they need.
+//!
+//! Fortunately, every present is a box (a perfect right rectangular prism),
+//! which makes calculating the required wrapping paper for each gift a little easier:
+//! find the surface area of the box, which is `2*l*w + 2*w*h + 2*h*l`.
+//! The elves also need a little extra paper for each present: the area of the smallest side.
+//!
+//! For example:
+//!
+//! * A present with dimensions `2x3x4` requires `2*6 + 2*12 + 2*8 = 52` square
+//! feet of wrapping paper plus `6` square feet of slack, for a total of `58` square feet.
+//! * A present with dimensions `1x1x10` requires `2*1 + 2*10 + 2*10 = 42` square feet
+//! of wrapping paper plus `1` square foot of slack, for a total of `43` square feet.
+//!
+//! All numbers in the elves' list are in feet. How many total square feet of wrapping
+//! paper should they order?
+//!
+//! **--- Part Two ---**
+//!
+//! The elves are also running low on ribbon. Ribbon is all the same width, so they
+//! only have to worry about the length they need to order, which they would again like
+//! to be exact.
+//!
+//! The ribbon required to wrap a present is the shortest distance around its sides,
+//! or the smallest perimeter of any one face. Each present also requires a bow made out
+//! of ribbon as well; the feet of ribbon required for the perfect bow is equal to the
+//! cubic feet of volume of the present. Don't ask how they tie the bow, though; they'll never tell.
+//!
+//! For example:
+//!
+//! * A present with dimensions `2x3x4` requires `2+2+3+3 = 10` feet of ribbon to wrap the
+//! present plus `2*3*4 = 24` feet of ribbon for the bow, for a total of `34` feet.
+//! * A present with dimensions `1x1x10` requires `1+1+1+1 = 4` feet of ribbon to wrap the
+//! present plus `1*1*10 = 10` feet of ribbon for the bow, for a total of `14` feet.
+//!
+//! How many total feet of ribbon should they order?
+//!
+use crate::{
+    constants::{AoCDay, AoCYear},
+    utils::run_solution,
+};
+use anyhow::Result;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
 /// Solution for Part 1
 ///
@@ -17,7 +69,52 @@ use anyhow::{anyhow, Result};
 /// [`AoCDay`](crate::constants::AoCDay) cannot be read.
 /// * This function will error if the elapsed [`std::time::Duration`] is invalid.
 pub fn part_1() -> Result<u32> {
-    Err(anyhow!("not implemented"))
+    run_solution::<usize>(AoCYear::AOC2015, AoCDay::AOCD02, find_area).map(|_| 0)
+}
+
+fn find_area(reader: BufReader<File>) -> usize {
+    find_area_br(reader)
+}
+
+fn find_area_br<T>(reader: T) -> usize
+where
+    T: BufRead,
+{
+    reader
+        .lines()
+        .filter_map(std::result::Result::ok)
+        .scan(0, handle_line_p1)
+        .last()
+        .unwrap_or_default()
+}
+
+#[allow(clippy::needless_pass_by_value)]
+#[inline]
+fn handle_line_p1(acc: &mut usize, line: String) -> Option<usize> {
+    let parts = line.split('x').collect::<Vec<&str>>();
+    if parts.len() == 3 {
+        let l_res = str::parse::<usize>(parts[0]);
+        let w_res = str::parse::<usize>(parts[1]);
+        let h_res = str::parse::<usize>(parts[2]);
+
+        if let (Ok(l), Ok(w), Ok(h)) = (l_res, w_res, h_res) {
+            let mut areas = vec![];
+            areas.push(l * w);
+            areas.push(w * h);
+            areas.push(h * l);
+
+            areas.iter().min().map(|min| {
+                let total_area: usize = areas.iter().map(|x| 2 * x).sum();
+                let total = total_area + min;
+                *acc += total;
+                *acc
+            })
+        } else {
+            None
+        }
+    } else {
+        None
+    }
 }
 
 /// Solution for Part 2
@@ -27,5 +124,82 @@ pub fn part_1() -> Result<u32> {
 /// [`AoCDay`](crate::constants::AoCDay) cannot be read.
 /// * This function will error if the elapsed [`std::time::Duration`] is invalid.
 pub fn part_2() -> Result<u32> {
-    Err(anyhow!("not implemented"))
+    run_solution::<usize>(AoCYear::AOC2015, AoCDay::AOCD02, find_length).map(|_| 0)
+}
+
+fn find_length(reader: BufReader<File>) -> usize {
+    find_length_br(reader)
+}
+
+fn find_length_br<T>(reader: T) -> usize
+where
+    T: BufRead,
+{
+    reader
+        .lines()
+        .filter_map(std::result::Result::ok)
+        .scan(0, handle_line_p2)
+        .last()
+        .unwrap_or_default()
+}
+
+#[allow(clippy::needless_pass_by_value)]
+#[inline]
+fn handle_line_p2(acc: &mut usize, line: String) -> Option<usize> {
+    let parts = line.split('x').collect::<Vec<&str>>();
+    if parts.len() == 3 {
+        let l_res = str::parse::<usize>(parts[0]);
+        let w_res = str::parse::<usize>(parts[1]);
+        let h_res = str::parse::<usize>(parts[2]);
+
+        if let (Ok(l), Ok(w), Ok(h)) = (l_res, w_res, h_res) {
+            let mut perims = vec![];
+            perims.push(2 * (l + w));
+            perims.push(2 * (w + h));
+            perims.push(2 * (h + l));
+
+            perims.iter().min().map(|min| {
+                let total = (l * w * h) + min;
+                *acc += total;
+                *acc
+            })
+        } else {
+            None
+        }
+    } else {
+        None
+    }
+}
+#[cfg(test)]
+mod one_star {
+    use super::find_area_br;
+    use anyhow::Result;
+    use std::io::Cursor;
+
+    const TEST_1: &str = r"2x3x4";
+    const TEST_2: &str = r"1x1x10";
+
+    #[test]
+    fn solution() -> Result<()> {
+        assert_eq!(find_area_br(Cursor::new(TEST_1)), 58);
+        assert_eq!(find_area_br(Cursor::new(TEST_2)), 43);
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod two_star {
+    use super::find_length_br;
+    use anyhow::Result;
+    use std::io::Cursor;
+
+    const TEST_1: &str = r"2x3x4";
+    const TEST_2: &str = r"1x1x10";
+
+    #[test]
+    fn solution() -> Result<()> {
+        assert_eq!(find_length_br(Cursor::new(TEST_1)), 34);
+        assert_eq!(find_length_br(Cursor::new(TEST_2)), 14);
+        Ok(())
+    }
 }
--- aoc2-sol/src/year2015/day03.rs
+++ aoc2-sol/src/year2015/day03.rs
@@ -6,9 +6,58 @@
 // option. All files in the project carrying such notice may not be copied,
 // modified, or distributed except according to those terms.
 
-//! Advent of Code - Day 3
+//! Advent of Code - Day 3 "Perfectly Spherical Houses in a Vacuum"
+//!
+//! **--- Day 3: Perfectly Spherical Houses in a Vacuum ---**
+//!
+//! **--- Part 1 ---**
+//!
+//! Santa is delivering presents to an infinite two-dimensional grid of houses.
+//!
+//! He begins by delivering a present to the house at his starting location,
+//! and then an elf at the North Pole calls him via radio and tells him where
+//! to move next. Moves are always exactly one house to the north (^), south (v),
+//! east (>), or west (<). After each move, he delivers another present to the
+//! house at his new location.
+//!
+//! However, the elf back at the north pole has had a little too much eggnog,
+//! and so his directions are a little off, and Santa ends up visiting some houses
+//! more than once. How many houses receive at least one present?
+//!
+//! For example:
+//!
+//! * `>` delivers presents to `2` houses: one at the starting location, and one to the east.
+//! * `^>v<` delivers presents to `4` houses in a square, including twice to the
+//! house at his starting/ending location.
+//! * `^v^v^v^v^v` delivers a bunch of presents to some very lucky children at only 2 houses.
+//!
+//! **--- Part Two ---**
+//!
+//! The next year, to speed up the process, Santa creates a robot version of himself,
+//! Robo-Santa, to deliver presents with him.
+//!
+//! Santa and Robo-Santa start at the same location (delivering two presents to the
+//! same starting house), then take turns moving based on instructions from the elf,
+//! who is eggnoggedly reading from the same script as the previous year.
+//!
+//! This year, how many houses receive at least one present?
+//!
+//! For example:
+//!
+//! * `^v` delivers presents to `3` houses, because Santa goes north, and then Robo-Santa goes south.
+//! * `^>v<` now delivers presents to 3 houses, and Santa and Robo-Santa end up back where they started.
+//! * `^v^v^v^v^v` now delivers presents to 11 houses, with Santa going one direction and Robo-Santa going the other.
 
+use crate::{
+    constants::{AoCDay, AoCYear},
+    utils::run_solution,
+};
 use anyhow::{anyhow, Result};
+use std::{
+    collections::HashMap,
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
 /// Solution for Part 1
 ///
@@ -17,7 +66,55 @@ use anyhow::{anyhow, Result};
 /// [`AoCDay`](crate::constants::AoCDay) cannot be read.
 /// * This function will error if the elapsed [`std::time::Duration`] is invalid.
 pub fn part_1() -> Result<u32> {
-    Err(anyhow!("not implemented"))
+    run_solution::<usize>(AoCYear::AOC2015, AoCDay::AOCD03, find).map(|_| 0)
+}
+
+fn find(reader: BufReader<File>) -> usize {
+    find_br(reader).unwrap_or_default()
+}
+
+fn find_br<T>(reader: T) -> Result<usize>
+where
+    T: BufRead,
+{
+    let mut house_map = HashMap::new();
+
+    for line in reader.lines().filter_map(std::result::Result::ok) {
+        start_delivery(&mut house_map, line.chars())?;
+    }
+
+    Ok(house_map.values().filter(|x| **x > 0).count())
+}
+
+fn start_delivery(
+    house_map: &mut HashMap<(isize, isize), usize>,
+    iter: impl Iterator<Item = char>,
+) -> Result<()> {
+    let mut x: isize = 0;
+    let mut y: isize = 0;
+    *house_map.entry((x, y)).or_insert(0_usize) += 1;
+    for ch in iter {
+        deliver(house_map, &mut x, &mut y, ch)?;
+    }
+    Ok(())
+}
+
+fn deliver(
+    house_map: &mut HashMap<(isize, isize), usize>,
+    x: &mut isize,
+    y: &mut isize,
+    ch: char,
+) -> Result<()> {
+    match ch {
+        '^' => *y += 1,
+        '>' => *x += 1,
+        'v' => *y -= 1,
+        '<' => *x -= 1,
+        _ => return Err(anyhow!("invalid movement")),
+    }
+
+    *house_map.entry((*x, *y)).or_insert(0) += 1;
+    Ok(())
 }
 
 /// Solution for Part 2
@@ -27,5 +124,64 @@ pub fn part_1() -> Result<u32> {
 /// [`AoCDay`](crate::constants::AoCDay) cannot be read.
 /// * This function will error if the elapsed [`std::time::Duration`] is invalid.
 pub fn part_2() -> Result<u32> {
-    Err(anyhow!("not implemented"))
+    run_solution::<usize>(AoCYear::AOC2015, AoCDay::AOCD03, find2).map(|_| 0)
+}
+
+fn find2(reader: BufReader<File>) -> usize {
+    find2_br(reader).unwrap_or_default()
+}
+
+fn find2_br<T>(reader: T) -> Result<usize>
+where
+    T: BufRead,
+{
+    let mut house_map = HashMap::new();
+
+    for line in reader.lines().filter_map(std::result::Result::ok) {
+        let santa_iter = line.chars().step_by(2);
+        let robo_iter = line.chars().skip(1).step_by(2);
+
+        start_delivery(&mut house_map, santa_iter)?;
+        start_delivery(&mut house_map, robo_iter)?;
+    }
+
+    Ok(house_map.values().filter(|x| **x > 0).count())
+}
+
+#[cfg(test)]
+mod one_star {
+    use super::find_br;
+    use anyhow::Result;
+    use std::io::Cursor;
+
+    const TEST_1: &str = r">";
+    const TEST_2: &str = r"^>v<";
+    const TEST_3: &str = r"^v^v^v^v^v";
+
+    #[test]
+    fn solution() -> Result<()> {
+        assert_eq!(find_br(Cursor::new(TEST_1))?, 2);
+        assert_eq!(find_br(Cursor::new(TEST_2))?, 4);
+        assert_eq!(find_br(Cursor::new(TEST_3))?, 2);
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod two_star {
+    use super::find2_br;
+    use anyhow::Result;
+    use std::io::Cursor;
+
+    const TEST_1: &str = r"^v";
+    const TEST_2: &str = r"^>v<";
+    const TEST_3: &str = r"^v^v^v^v^v";
+
+    #[test]
+    fn solution() -> Result<()> {
+        assert_eq!(find2_br(Cursor::new(TEST_1))?, 3);
+        assert_eq!(find2_br(Cursor::new(TEST_2))?, 3);
+        assert_eq!(find2_br(Cursor::new(TEST_3))?, 11);
+        Ok(())
+    }
 }
