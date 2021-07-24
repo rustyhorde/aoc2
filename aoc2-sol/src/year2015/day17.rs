7f396ef41d14386012612e279af84864e7669285
Jason Ozias <jason.ozias@kroger.com>
2021-07-23 22:26:51 -0400

    AoC 2015 Day 17

--- aoc2-sol/src/year2015/day17.rs
+++ aoc2-sol/src/year2015/day17.rs
@@ -6,13 +6,49 @@
 // option. All files in the project carrying such notice may not be copied,
 // modified, or distributed except according to those terms.
 
-//! Advent of Code - Day 17
+//! Advent of Code - Day 17 "No Such Thing as Too Much"
+//!
+//! **--- Day 17: No Such Thing as Too Much ---**
+//!
+//! **--- Part 1 ---**
+//!
+//! The elves bought too much eggnog again - 150 liters this time. To fit it all
+//! into your refrigerator, you'll need to move it into smaller containers.
+//! You take an inventory of the capacities of the available containers.
+//!
+//! For example, suppose you have containers of size `20`, `15`, `10`, `5`,
+//! and `5` liters. If you need to store `25` liters, there are four ways to do it:
+//!
+//! ```text
+//! 15 and 10
+//! 20 and 5 (the first 5)
+//! 20 and 5 (the second 5)
+//! 15, 5, and 5
+//! ```
+//!
+//! Filling all containers entirely, how many different combinations of
+//! containers can exactly fit all 150 liters of eggnog?
+//!
+//! **--- Part Two ---**
+//!
+//! While playing with all the containers in the kitchen, another load of
+//! eggnog arrives! The shipping and receiving department is requesting as many
+//! containers as you can spare.
+//!
+//! Find the minimum number of containers that can exactly fit all `150` liters of
+//! eggnog. How many different ways can you fill that number of containers and still
+//! hold exactly 150 litres?
+//!
+//! In the example above, the minimum number of containers was two. There were
+//! three ways to use that many containers, and so the answer there would be 3.
+//!
 
 use crate::{
     constants::{AoCDay, AoCYear},
     utils::{run_solution, valid_lines},
 };
 use anyhow::Result;
+use itertools::Itertools;
 use std::{
     fs::File,
     io::{BufRead, BufReader},
@@ -29,15 +65,27 @@ pub fn part_1() -> Result<u32> {
 }
 
 fn find(reader: BufReader<File>) -> usize {
-    find_br(reader)
+    find_br(reader, 150).unwrap_or_default()
 }
 
-fn find_br<T>(reader: T) -> usize
+fn find_br<T>(reader: T, amount: usize) -> Result<usize>
 where
     T: BufRead,
 {
-    for _line in valid_lines(reader) {}
-    0
+    let mut containers = vec![];
+
+    for line in valid_lines(reader) {
+        let val = line.parse::<usize>()?;
+        containers.push(val);
+    }
+
+    let mut results = vec![];
+    for ps in containers.into_iter().powerset() {
+        if ps.iter().sum::<usize>() == amount {
+            results.push(ps);
+        }
+    }
+    Ok(results.len())
 }
 
 /// Solution for Part 2
@@ -51,49 +99,70 @@ pub fn part_2() -> Result<u32> {
 }
 
 fn find2(reader: BufReader<File>) -> usize {
-    find2_br(reader)
+    find2_br(reader, 150).unwrap_or_default()
 }
 
-fn find2_br<T>(reader: T) -> usize
+fn find2_br<T>(reader: T, amount: usize) -> Result<usize>
 where
     T: BufRead,
 {
-    for _line in valid_lines(reader) {}
-    0
+    let mut containers = vec![];
+
+    for line in valid_lines(reader) {
+        let val = line.parse::<usize>()?;
+        containers.push(val);
+    }
+
+    let mut results = vec![];
+    for ps in containers.into_iter().powerset() {
+        if ps.iter().sum::<usize>() == amount {
+            results.push(ps);
+        }
+    }
+    let mut min = usize::MAX;
+    for blah in &results {
+        if blah.len() < min {
+            min = blah.len();
+        }
+    }
+    let num_min = results.iter().filter(|a| a.len() == min).count();
+    Ok(num_min)
 }
 
 #[cfg(test)]
 mod one_star {
-    // use super::find_br;
+    use super::find_br;
     use anyhow::Result;
-    // use std::io::Cursor;
+    use std::io::Cursor;
 
-    // const TEST_1: &str = r"turn on 0,0 through 999,999";
-    // const TEST_2: &str = r"toggle 0,0 through 999,0";
-    // const TEST_3: &str = r"turn on 0,0 through 999,999\nturn off 499,499 through 500,500";
+    const TEST_1: &str = r"20
+15
+10
+5
+5";
 
     #[test]
     fn solution() -> Result<()> {
-        // assert_eq!(find_br(Cursor::new(TEST_1))?, 1_000_000);
-        // assert_eq!(find_br(Cursor::new(TEST_2))?, 1_000);
-        // assert_eq!(find_br(Cursor::new(TEST_3))?, 999_996);
+        assert_eq!(find_br(Cursor::new(TEST_1), 25)?, 4);
         Ok(())
     }
 }
 
 #[cfg(test)]
 mod two_star {
-    // use super::find2_br;
+    use super::find2_br;
     use anyhow::Result;
-    // use std::io::Cursor;
+    use std::io::Cursor;
 
-    // const TEST_1: &str = r"turn on 0,0 through 0,0";
-    // const TEST_2: &str = r"toggle 0,0 through 999,999";
+    const TEST_1: &str = r"20
+15
+10
+5
+5";
 
     #[test]
     fn solution() -> Result<()> {
-        // assert_eq!(find2_br(Cursor::new(TEST_1))?, 1);
-        // assert_eq!(find2_br(Cursor::new(TEST_2))?, 2_000_000);
+        assert_eq!(find2_br(Cursor::new(TEST_1), 25)?, 3);
         Ok(())
     }
 }
