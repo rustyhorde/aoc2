9bf32b11ac63cbbf4f8b297188d268e46296e243
Jason Ozias <jason.ozias@kroger.com>
2021-07-26 23:00:30 -0400

    AoC 2016 Day 06

--- aoc2-sol/src/year2016/day06.rs
+++ aoc2-sol/src/year2016/day06.rs
@@ -6,14 +6,73 @@
 // option. All files in the project carrying such notice may not be copied,
 // modified, or distributed except according to those terms.
 
-//! Advent of Code - Day 6
+//! Advent of Code - Day 6 "Signals and Noise"
+//!
+//! **--- Day 6: Signals and Noise ---**
+//!
+//! **--- Part 1 ---**
+//!
+//! Something is jamming your communications with Santa. Fortunately, your signal
+//! is only partially jammed, and protocol in situations like this is to switch to a
+//! simple repetition code to get the message through.
+//!
+//! In this model, the same message is sent repeatedly. You've recorded the
+//! repeating message signal (your puzzle input), but the data seems quite corrupted -
+//! almost too badly to recover. Almost.
+//!
+//! All you need to do is figure out which character is most frequent for each position.
+//! For example, suppose you had recorded the following messages:
+//!
+//! ```text
+//! eedadn
+//! drvtee
+//! eandsr
+//! raavrd
+//! atevrs
+//! tsrnev
+//! sdttsa
+//! rasrtv
+//! nssdts
+//! ntnada
+//! svetve
+//! tesnvt
+//! vntsnd
+//! vrdear
+//! dvrsen
+//! enarar
+//! ```
+//!
+//! The most common character in the first column is e; in the second, a; in the third, s,
+//! and so on. Combining these characters returns the error-corrected message, `easter`.
+//!
+//! Given the recording in your puzzle input, what is the error-corrected version of the message being sent?
+//!
+//! **--- Part Two ---**
+//!
+//! Of course, that would be the message - if you hadn't agreed to use a modified repetition
+//! code instead.
+//!
+//! In this modified code, the sender instead transmits what looks like random data, but
+//! for each character, the character they actually want to send is slightly less likely
+//! than the others. Even after signal-jamming noise, you can look at the letter distributions
+//! in each column and choose the least common letter to reconstruct the original message.
+//!
+//! In the above example, the least common character in the first column is a; in the second,
+//! d, and so on. Repeating this process for the remaining characters produces the original message,
+//! `advent`.
+//!
+//! Given the recording in your puzzle input and this new decoding methodology, what is the
+//! original message that Santa is trying to send?
 
 use crate::{
     constants::{AoCDay, AoCYear},
     utils::{run_solution, valid_lines},
 };
 use anyhow::Result;
+use itertools::Itertools;
 use std::{
+    cmp::Ordering,
+    collections::{BTreeMap, HashMap},
     fs::File,
     io::{BufRead, BufReader},
 };
@@ -25,19 +84,50 @@ use std::{
 /// [`AoCDay`](crate::constants::AoCDay) cannot be read.
 /// * This function will error if the elapsed [`std::time::Duration`] is invalid.
 pub fn part_1() -> Result<u32> {
-    run_solution::<usize>(AoCYear::AOC2016, AoCDay::AOCD06, find).map(|_| 0)
+    run_solution::<String>(AoCYear::AOC2016, AoCDay::AOCD06, find).map(|_| 0)
 }
 
-fn find(reader: BufReader<File>) -> usize {
+fn find(reader: BufReader<File>) -> String {
     find_br(reader)
 }
 
-fn find_br<T>(reader: T) -> usize
+fn find_br<T>(reader: T) -> String
 where
     T: BufRead,
 {
-    for _line in valid_lines(reader) {}
-    0
+    let mut freq_map = BTreeMap::new();
+    for line in valid_lines(reader) {
+        for (idx, ch) in line.chars().enumerate() {
+            let idx_e = freq_map.entry(idx).or_insert_with(HashMap::new);
+            *idx_e.entry(ch).or_insert(0) += 1;
+        }
+    }
+
+    let tupled = freq_map
+        .iter()
+        .map(|(k, v)| {
+            (
+                *k,
+                v.iter()
+                    .map(|(k, v)| (*k, *v))
+                    .sorted_by(|(ch1, c1), (ch2, c2)| {
+                        if c2.cmp(c1) == Ordering::Equal {
+                            ch1.cmp(ch2)
+                        } else {
+                            c2.cmp(c1)
+                        }
+                    })
+                    .collect(),
+            )
+        })
+        .collect::<BTreeMap<usize, Vec<(char, usize)>>>();
+
+    let mut result = String::new();
+
+    for (_k, v) in tupled {
+        result.push(v[0].0);
+    }
+    result
 }
 
 /// Solution for Part 2
@@ -47,19 +137,50 @@ where
 /// [`AoCDay`](crate::constants::AoCDay) cannot be read.
 /// * This function will error if the elapsed [`std::time::Duration`] is invalid.
 pub fn part_2() -> Result<u32> {
-    run_solution::<usize>(AoCYear::AOC2016, AoCDay::AOCD06, find2).map(|_| 0)
+    run_solution::<String>(AoCYear::AOC2016, AoCDay::AOCD06, find2).map(|_| 0)
 }
 
-fn find2(reader: BufReader<File>) -> usize {
+fn find2(reader: BufReader<File>) -> String {
     find2_br(reader)
 }
 
-fn find2_br<T>(reader: T) -> usize
+fn find2_br<T>(reader: T) -> String
 where
     T: BufRead,
 {
-    for _line in valid_lines(reader) {}
-    0
+    let mut freq_map = BTreeMap::new();
+    for line in valid_lines(reader) {
+        for (idx, ch) in line.chars().enumerate() {
+            let idx_e = freq_map.entry(idx).or_insert_with(HashMap::new);
+            *idx_e.entry(ch).or_insert(0) += 1;
+        }
+    }
+
+    let tupled = freq_map
+        .iter()
+        .map(|(k, v)| {
+            (
+                *k,
+                v.iter()
+                    .map(|(k, v)| (*k, *v))
+                    .sorted_by(|(ch1, c1), (ch2, c2)| {
+                        if c1.cmp(c2) == Ordering::Equal {
+                            ch1.cmp(ch2)
+                        } else {
+                            c1.cmp(c2)
+                        }
+                    })
+                    .collect(),
+            )
+        })
+        .collect::<BTreeMap<usize, Vec<(char, usize)>>>();
+
+    let mut result = String::new();
+
+    for (_k, v) in tupled {
+        result.push(v[0].0);
+    }
+    result
 }
 
 #[cfg(test)]
@@ -68,28 +189,56 @@ mod one_star {
     use anyhow::Result;
     use std::io::Cursor;
 
-    const TEST_1: &str = r">";
+    const TEST_1: &str = r"eedadn
+drvtee
+eandsr
+raavrd
+atevrs
+tsrnev
+sdttsa
+rasrtv
+nssdts
+ntnada
+svetve
+tesnvt
+vntsnd
+vrdear
+dvrsen
+enarar";
 
     #[test]
     fn solution() -> Result<()> {
-        assert_eq!(find_br(Cursor::new(TEST_1)), 0);
+        assert_eq!(find_br(Cursor::new(TEST_1)), "easter");
         Ok(())
     }
 }
 
 #[cfg(test)]
 mod two_star {
-    // use super::find2_br;
+    use super::find2_br;
     use anyhow::Result;
-    // use std::io::Cursor;
+    use std::io::Cursor;
 
-    // const TEST_1: &str = r"^v";
-    // const TEST_2: &str = r"^>v<";
-    // const TEST_3: &str = r"^v^v^v^v^v";
+    const TEST_1: &str = r"eedadn
+drvtee
+eandsr
+raavrd
+atevrs
+tsrnev
+sdttsa
+rasrtv
+nssdts
+ntnada
+svetve
+tesnvt
+vntsnd
+vrdear
+dvrsen
+enarar";
 
     #[test]
     fn solution() -> Result<()> {
-        // assert_eq!(find2_br(Cursor::new(TEST_1))?, 3);
+        assert_eq!(find2_br(Cursor::new(TEST_1)), "advent");
         Ok(())
     }
 }
