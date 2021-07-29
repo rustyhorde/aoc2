2529bd95cc1aa1c124c5e87da8d9babfe7549e96
Jason Ozias <jason.ozias@kroger.com>
2021-07-29 18:07:35 -0400

    AoC 2016 Day 11 inprogress

--- aoc2-sol/src/year2016/day11.rs
+++ aoc2-sol/src/year2016/day11.rs
@@ -6,17 +6,308 @@
 // option. All files in the project carrying such notice may not be copied,
 // modified, or distributed except according to those terms.
 
-//! Advent of Code - Day 11
+//! Advent of Code - Day 11 "Radioisotope Thermoelectric Generators"
+//!
+//! **--- Day 11: Radioisotope Thermoelectric Generators ---**
+//!
+//! **--- Part 1 ---**
+//!
+//! You come upon a column of four floors that have been entirely sealed off
+//! from the rest of the building except for a small dedicated lobby. There
+//! are some radiation warnings and a big sign which reads "Radioisotope
+//! Testing Facility".
+//!
+//! According to the project status board, this facility is currently being
+//! used to experiment with Radioisotope Thermoelectric Generators (RTGs, or
+//! simply "generators") that are designed to be paired with specially-constructed
+//! microchips. Basically, an RTG is a highly radioactive rock that generates
+//! electricity through heat.
+//!
+//! The experimental RTGs have poor radiation containment, so they're dangerously
+//! radioactive. The chips are prototypes and don't have normal radiation shielding,
+//! but they do have the ability to generate an electromagnetic radiation shield
+//! when powered. Unfortunately, they can only be powered by their corresponding
+//! RTG. An RTG powering a microchip is still dangerous to other microchips.
+//!
+//! In other words, if a chip is ever left in the same area as another RTG, and
+//! it's not connected to its own RTG, the chip will be fried. Therefore, it is
+//! assumed that you will follow procedure and keep chips connected to their
+//! corresponding RTG when they're in the same room, and away from other RTGs otherwise.
+//!
+//! These microchips sound very interesting and useful to your current activities,
+//! and you'd like to try to retrieve them. The fourth floor of the facility has
+//! an assembling machine which can make a self-contained, shielded computer for you
+//! to take with you - that is, if you can bring it all of the RTGs and microchips.
+//!
+//! Within the radiation-shielded part of the facility (in which it's safe to have
+//! these pre-assembly RTGs), there is an elevator that can move between the four
+//! floors. Its capacity rating means it can carry at most yourself and two RTGs or
+//! microchips in any combination. (They're rigged to some heavy diagnostic equipment -
+//! the assembling machine will detach it for you.) As a security measure, the
+//! elevator will only function if it contains at least one RTG or microchip. The
+//! elevator always stops on each floor to recharge, and this takes long enough that
+//! the items within it and the items on that floor can irradiate each other.
+//! (You can prevent this if a Microchip and its Generator end up on the same floor
+//! in this way, as they can be connected while the elevator is recharging.)
+//!
+//! You make some notes of the locations of each component of interest (your puzzle
+//! input). Before you don a hazmat suit and start moving things around, you'd like
+//! to have an idea of what you need to do.
+//!
+//! When you enter the containment area, you and the elevator will start on the first floor.
+//!
+//! For example, suppose the isolated area has the following arrangement:
+//!
+//! ```text
+//! The first floor contains a hydrogen-compatible microchip and a lithium-compatible microchip.
+//! The second floor contains a hydrogen generator.
+//! The third floor contains a lithium generator.
+//! The fourth floor contains nothing relevant.
+//! ```
+//!
+//! As a diagram (F# for a Floor number, E for Elevator, H for Hydrogen, L for Lithium,
+//! M for Microchip, and G for Generator), the initial state looks like this:
+//!
+//! ```text
+//! F4 .  .  .  .  .
+//! F3 .  .  .  LG .
+//! F2 .  HG .  .  .
+//! F1 E  .  HM .  LM
+//! ```
+//!
+//! Then, to get everything up to the assembling machine on the fourth floor, the
+//! following steps could be taken:
+//!
+//! Bring the Hydrogen-compatible Microchip to the second floor, which is safe
+//! because it can get power from the Hydrogen Generator:
+//!
+//! ```text
+//! F4 .  .  .  .  .
+//! F3 .  .  .  LG .
+//! F2 E  HG HM .  .
+//! F1 .  .  .  .  LM
+//! ```
+//!
+//! Bring both Hydrogen-related items to the third floor, which is safe because the
+//! Hydrogen-compatible microchip is getting power from its generator:
+//!
+//! ```text
+//! F4 .  .  .  .  .
+//! F3 E  HG HM LG .
+//! F2 .  .  .  .  .
+//! F1 .  .  .  .  LM
+//! ```
+//!
+//! Leave the Hydrogen Generator on floor three, but bring the Hydrogen-compatible
+//! Microchip back down with you so you can still use the elevator:
+//!
+//! ```text
+//! F4 .  .  .  .  .
+//! F3 .  HG .  LG .
+//! F2 E  .  HM .  .
+//! F1 .  .  .  .  LM
+//! ```
+//!
+//! At the first floor, grab the Lithium-compatible Microchip, which is safe because
+//! Microchips don't affect each other:
+//!
+//! ```text
+//! F4 .  .  .  .  .
+//! F3 .  HG .  LG .
+//! F2 .  .  .  .  .
+//! F1 E  .  HM .  LM
+//! ```
+//!
+//! Bring both Microchips up one floor, where there is nothing to fry them:
+//!
+//! ```text
+//! F4 .  .  .  .  .
+//! F3 .  HG .  LG .
+//! F2 E  .  HM .  LM
+//! F1 .  .  .  .  .
+//! ```
+//!
+//! Bring both Microchips up again to floor three, where they can be temporarily
+//! connected to their corresponding generators while the elevator recharges,
+//! preventing either of them from being fried:
+//!
+//! ```text
+//! F4 .  .  .  .  .
+//! F3 E  HG HM LG LM
+//! F2 .  .  .  .  .
+//! F1 .  .  .  .  .
+//! ```
+//!
+//! Bring both Microchips to the fourth floor:
+//!
+//! ```text
+//! F4 E  .  HM .  LM
+//! F3 .  HG .  LG .
+//! F2 .  .  .  .  .
+//! F1 .  .  .  .  .
+//! ```
+//!
+//! Leave the Lithium-compatible microchip on the fourth floor, but bring the
+//! Hydrogen-compatible one so you can still use the elevator; this is safe because
+//! although the Lithium Generator is on the destination floor, you can connect
+//! Hydrogen-compatible microchip to the Hydrogen Generator there:
+//!
+//! ```text
+//! F4 .  .  .  .  LM
+//! F3 E  HG HM LG .
+//! F2 .  .  .  .  .
+//! F1 .  .  .  .  .
+//! ```
+//!
+//! Bring both Generators up to the fourth floor, which is safe because you can
+//! connect the Lithium-compatible Microchip to the Lithium Generator upon arrival:
+//!
+//! ```text
+//! F4 E  HG .  LG LM
+//! F3 .  .  HM .  .
+//! F2 .  .  .  .  .
+//! F1 .  .  .  .  .
+//! ```
+//!
+//! Bring the Lithium Microchip with you to the third floor so you can use the elevator:
+//!
+//! ```text
+//! F4 .  HG .  LG .
+//! F3 E  .  HM .  LM
+//! F2 .  .  .  .  .
+//! F1 .  .  .  .  .
+//! ```
+//!
+//! Bring both Microchips to the fourth floor:
+//!
+//! ```text
+//! F4 E  HG HM LG LM
+//! F3 .  .  .  .  .
+//! F2 .  .  .  .  .
+//! F1 .  .  .  .  .
+//! ```
+//!
+//! In this arrangement, it takes 11 steps to collect all of the objects at the fourth
+//! floor for assembly. (Each elevator stop counts as one step, even if nothing is added
+//! to or removed from it.)
+//!
+//! In your situation, what is the minimum number of steps required to bring all of
+//! the objects to the fourth floor?
 
 use crate::{
     constants::{AoCDay, AoCYear},
-    utils::{run_solution, valid_lines},
+    utils::{get_cap, run_solution, valid_lines},
 };
-use anyhow::Result;
+use anyhow::{anyhow, Result};
+use indexmap::IndexSet;
+use itertools::Itertools;
+use regex::Regex;
 use std::{
+    cmp::Ordering,
+    collections::BTreeMap,
+    fmt,
     fs::File,
     io::{BufRead, BufReader},
 };
+
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialOrd, PartialEq)]
+enum ItemKind {
+    Generator,
+    Microchip,
+}
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+struct Item {
+    name: Element,
+    kind: ItemKind,
+}
+
+impl PartialOrd for Item {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Item {
+    fn cmp(&self, other: &Item) -> Ordering {
+        if self.name.cmp(&other.name) == Ordering::Equal {
+            self.kind.cmp(&other.kind)
+        } else {
+            self.name.cmp(&other.name)
+        }
+    }
+}
+
+impl fmt::Display for Item {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut element = String::new();
+        element.push_str(&self.name.0.to_ascii_uppercase()[0..1]);
+        element.push_str(&self.name.0[1..2]);
+        write!(
+            f,
+            "{}",
+            match self.kind {
+                ItemKind::Generator => format!("{}G", element),
+                ItemKind::Microchip => format!("{}M", element),
+            }
+        )
+    }
+}
+
+impl Item {
+    fn can_join(&self, items: &[Item]) -> bool {
+        let mut can_join = false;
+        match self.kind {
+            ItemKind::Generator => {
+                for item in items {
+                    if item.kind == ItemKind::Microchip && item.name != self.name {
+                        can_join = false;
+                        break;
+                    }
+                }
+            },
+            ItemKind::Microchip => {
+                for item in items {
+                    if item.kind == ItemKind::Generator && item.name == self.name {
+                        can_join = true;
+                        break;
+                    }
+                }
+
+                let mut has_another_generator = false;
+                for item in items {
+                    if item.kind == ItemKind::Generator && item.name != self.name {
+                        has_another_generator = true;
+                        break;
+                    }
+                }
+
+                if !has_another_generator {
+                    can_join = true;
+                }
+            }
+        }
+        can_join
+    }
+}
+
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+struct Element(String);
+
+#[derive(Clone, Copy, Debug)]
+enum Direction {
+    Up,
+    Down,
+}
+
+#[derive(Clone, Debug, Default)]
+struct Elevator {
+    item1: Option<Item>,
+    item2: Option<Item>,
+    floor: usize,
+    moves: usize,
+    direction: Option<Direction>,
+}
 
 /// Solution for Part 1
 ///
@@ -29,15 +320,270 @@ pub fn part_1() -> Result<u32> {
 }
 
 fn find(reader: BufReader<File>) -> usize {
-    find_br(reader)
+    find_br(reader).unwrap_or_default()
 }
 
-fn find_br<T>(reader: T) -> usize
+fn find_br<T>(reader: T) -> Result<usize>
 where
     T: BufRead,
 {
-    for _line in valid_lines(reader) {}
-    0
+    let floor_re = Regex::new(r"^The (.*) floor")?;
+    let generator_re = Regex::new(r"a [a-z]+ generator[ ,\.]")?;
+    let microchip_re = Regex::new(r"a [a-z]+-compatible[ ,\.]")?;
+    let mut floors = BTreeMap::new();
+    let mut elevator = Elevator::default();
+
+    for line in valid_lines(reader) {
+        let mut floor = 0;
+        for caps in floor_re.captures_iter(&line) {
+            let floor_str = get_cap(1, &caps)?;
+            floor = match &floor_str[..] {
+                "first" => 0,
+                "second" => 1,
+                "third" => 2,
+                "fourth" => 3,
+                _ => return Err(anyhow!(format!("invalid floor: {}", floor_str))),
+            };
+            let _ = floors.entry(floor).or_insert_with(Vec::<Item>::new);
+        }
+        for m in generator_re.find_iter(&line) {
+            let generator_match = &line[m.start()..m.end()];
+            let split = generator_match.split(' ').collect::<Vec<&str>>();
+            let generator = split[1].to_string();
+            let floor_vec = floors.entry(floor).or_default();
+            floor_vec.push(Item {
+                name: Element(generator),
+                kind: ItemKind::Generator,
+            });
+        }
+        for m in microchip_re.find_iter(&line) {
+            let microchip_match = &line[m.start()..m.end()];
+            let split = microchip_match.split(' ').collect::<Vec<&str>>();
+            let microchip =
+                split[1]
+                    .chars()
+                    .take_while(|c| *c != '-')
+                    .fold(String::new(), |acc, ch| {
+                        let mut val = acc;
+                        val.push(ch);
+                        val
+                    });
+            let floor_vec = floors.entry(floor).or_default();
+            floor_vec.push(Item {
+                name: Element(microchip),
+                kind: ItemKind::Microchip,
+            });
+        }
+    }
+
+    current_state(&floors, &elevator);
+    let mut item_set = IndexSet::new();
+    for items in floors.values() {
+        for item in items {
+            let _ = item_set.insert(item.clone());
+        }
+    }
+    item_set.sort();
+    let mut break_after = 2;
+    let results = all_moves(&mut floors, &mut elevator, item_set.len(), &mut break_after)?;
+    println!("Results: {}", results.len());
+    Ok(0)
+}
+
+fn all_moves(
+    floors: &mut BTreeMap<usize, Vec<Item>>,
+    elevator: &mut Elevator,
+    item_count: usize,
+    break_me: &mut usize,
+) -> Result<Vec<Elevator>> {
+    let valid_moves = find_valid_moves(&floors, &elevator)?;
+    let mut made_it = vec![];
+    for elevator in valid_moves {
+        let mut floors_clone = floors.clone();
+        let mut elevator_clone = elevator.clone();
+        move_floors(&mut floors_clone, &mut elevator_clone);
+        current_state(&floors_clone, &elevator_clone);
+
+        *break_me -= 1;
+
+        if *break_me == 0 {
+            break;
+        }
+        if let Some(items_on_4) = floors.get(&3) {
+            if items_on_4.len() == item_count {
+                made_it.push(elevator_clone);
+            } else {
+                made_it.extend(all_moves(
+                    &mut floors_clone,
+                    &mut elevator_clone,
+                    item_count,
+                    break_me,
+                )?);
+            }
+        } else {
+            return Err(anyhow!("invalid 4th floor"));
+        }
+    }
+    Ok(made_it)
+}
+
+fn move_floors(floors: &mut BTreeMap<usize, Vec<Item>>, elevator: &mut Elevator) {
+    // Remove the item from the current floor, then move the elevator and
+    // item accordingly
+    let curr_floor = elevator.floor;
+
+    match elevator.direction {
+        Some(Direction::Up) => {
+            if let Some(item) = &elevator.item1 {
+                floors.entry(curr_floor).or_default().retain(|f| f != item);
+                floors.entry(curr_floor + 1).or_default().push(item.clone());
+            }
+            elevator.floor += 1;
+            elevator.moves += 1;
+            elevator.direction = None;
+        }
+        Some(Direction::Down) => {
+            if let Some(item) = &elevator.item1 {
+                floors.entry(curr_floor).or_default().retain(|f| f != item);
+                floors.entry(curr_floor - 1).or_default().push(item.clone());
+            }
+            elevator.floor -= 1;
+            elevator.moves += 1;
+            elevator.direction = None;
+        }
+        _ => {}
+    }
+}
+
+fn find_valid_moves(
+    floors: &BTreeMap<usize, Vec<Item>>,
+    elevator: &Elevator,
+) -> Result<Vec<Elevator>> {
+    let mut possible = vec![];
+    let curr_floor = elevator.floor;
+    let curr_items = floors
+        .get(&curr_floor)
+        .ok_or_else(|| anyhow!("invalid floor"))?;
+
+    for combo in curr_items
+        .iter()
+        .powerset()
+        .filter(|x| !x.is_empty() && x.len() <= 2)
+    {
+        let len = combo.len();
+        let mut new_elevator = elevator.clone();
+        if len >= 1 {
+            new_elevator.item1 = Some(combo[0].clone());
+        }
+        if len == 2 {
+            new_elevator.item2 = Some(combo[1].clone());
+        }
+        possible.push(new_elevator);
+    }
+
+    can_move_up(&mut possible, floors, curr_floor)?;
+    can_move_down(&mut possible, floors, curr_floor)?;
+
+    // Filter out elevators stuck on curr floor
+    Ok(possible
+        .into_iter()
+        .filter(|x| x.direction.is_some())
+        .collect())
+}
+
+fn can_move_up(
+    elevators: &mut Vec<Elevator>,
+    floors: &BTreeMap<usize, Vec<Item>>,
+    curr_floor: usize,
+) -> Result<()> {
+    if curr_floor != 3 {
+        let above_items = floors
+            .get(&(curr_floor + 1))
+            .ok_or_else(|| anyhow!("invalid floor"))?;
+        for elevator in elevators {
+            let mut can_move_up = false;
+            if let Some(item1) = &elevator.item1 {
+                // Check item1 against above_items
+                can_move_up = item1.can_join(above_items);
+            }
+            if let Some(item2) = &elevator.item2 {
+                // Check item2 against above_items
+                can_move_up = item2.can_join(above_items);
+            }
+            if can_move_up {
+                elevator.direction = Some(Direction::Up);
+            }
+        }
+    }
+    Ok(())
+}
+
+fn can_move_down(
+    elevators: &mut Vec<Elevator>,
+    floors: &BTreeMap<usize, Vec<Item>>,
+    curr_floor: usize,
+) -> Result<()> {
+    if curr_floor != 0 {
+        let below_items = floors
+            .get(&(curr_floor - 1))
+            .ok_or_else(|| anyhow!("invalid floor"))?;
+        for elevator in elevators {
+            let mut can_move_up = false;
+            if let Some(item1) = &elevator.item1 {
+                // Check item1 against above_items
+                can_move_up = item1.can_join(below_items);
+            }
+            if let Some(item2) = &elevator.item2 {
+                // Check item2 against above_items
+                can_move_up = item2.can_join(below_items);
+            }
+            if can_move_up {
+                elevator.direction = Some(Direction::Down);
+            }
+        }
+    }
+    Ok(())
+}
+
+fn current_state(floors: &BTreeMap<usize, Vec<Item>>, elevator: &Elevator) {
+    let mut item_set = IndexSet::new();
+    for items in floors.values() {
+        for item in items {
+            let _ = item_set.insert(item.clone());
+        }
+    }
+    item_set.sort();
+    let max_width = item_set.len();
+
+    for i in (0..floors.len()).rev() {
+        let mut floor_str = String::new();
+        floor_str.push('F');
+        floor_str.push_str(&(i + 1).to_string());
+        floor_str.push(' ');
+
+        if elevator.floor == i {
+            floor_str.push('E');
+        } else {
+            floor_str.push('.');
+        }
+        floor_str.push_str("   ");
+
+        if let Some(items) = floors.get(&i) {
+            let mut str_rep: Vec<String> = Vec::new();
+            for _ in 0..max_width {
+                str_rep.push(".  ".to_string());
+            }
+
+            for item in items {
+                if let Some(idx) = item_set.get_index_of(item) {
+                    str_rep[idx] = item.to_string();
+                }
+            }
+            let row = str_rep.join(" ");
+            floor_str.push_str(&row);
+        }
+        println!("{}", floor_str);
+    }
 }
 
 /// Solution for Part 2
@@ -68,11 +614,14 @@ mod one_star {
     use anyhow::Result;
     use std::io::Cursor;
 
-    const TEST_1: &str = r">";
+    const TEST_1: &str = r"The first floor contains a hydrogen-compatible microchip and a lithium-compatible microchip.
+The second floor contains a hydrogen generator.
+The third floor contains a lithium generator.
+The fourth floor contains nothing relevant.";
 
     #[test]
     fn solution() -> Result<()> {
-        assert_eq!(find_br(Cursor::new(TEST_1)), 0);
+        assert_eq!(find_br(Cursor::new(TEST_1))?, 11);
         Ok(())
     }
 }
