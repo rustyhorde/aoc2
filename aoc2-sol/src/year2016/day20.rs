0ec7dda8e2d34695ff9afcae4b48d4e722c158ce
Jason Ozias <jason.ozias@kroger.com>
2021-08-02 17:16:30 -0400

    AoC 2016 Day 20

--- aoc2-sol/src/year2016/day20.rs
+++ aoc2-sol/src/year2016/day20.rs
@@ -6,14 +6,47 @@
 // option. All files in the project carrying such notice may not be copied,
 // modified, or distributed except according to those terms.
 
-//! Advent of Code - Day 20
+//! Firewall Rules
+//!
+//! **--- Day 20: Firewall Rules ---**
+//!
+//! **--- Part 1 ---**
+//!
+//! You'd like to set up a small hidden computer here so you can use
+//! it to get back into the network later. However, the corporate firewall
+//! only allows communication with certain external IP addresses.
+//!
+//! You've retrieved the list of blocked IPs from the firewall, but the list
+//! seems to be messy and poorly maintained, and it's not clear which IPs
+//! are allowed. Also, rather than being written in dot-decimal notation,
+//! they are written as plain 32-bit integers, which can have any value from
+//! `0` through `4294967295`, inclusive.
+//!
+//! For example, suppose only the values `0` through `9` were valid, and
+//! that you retrieved the following blacklist:
+//!
+//! ```text
+//! 5-8
+//! 0-2
+//! 4-7
+//! ````
+//!
+//! The blacklist specifies ranges of IPs (inclusive of both the start and end
+//! value) that are not allowed. Then, the only IPs that this firewall allows
+//! are `3` and `9`, since those are the only numbers not in any range.
+//!
+//! Given the list of blocked IPs you retrieved from the firewall (your puzzle
+//! input), what is the lowest-valued IP that is not blocked?
 
 use crate::{
     constants::{AoCDay, AoCYear},
-    utils::{run_solution, valid_lines},
+    utils::{get_cap_x, print_err, run_solution, valid_lines},
 };
 use anyhow::Result;
+use regex::Regex;
 use std::{
+    collections::BTreeMap,
+    convert::TryFrom,
     fs::File,
     io::{BufRead, BufReader},
 };
@@ -29,15 +62,56 @@ pub fn part_1() -> Result<u32> {
 }
 
 fn find(reader: BufReader<File>) -> usize {
-    find_br(reader)
+    find_br(reader, u32::MAX).map_err(print_err).unwrap_or_default()
 }
 
-fn find_br<T>(reader: T) -> usize
+fn find_br<T>(reader: T, max_value: u32) -> Result<usize>
 where
     T: BufRead,
 {
-    for _line in valid_lines(reader) {}
-    0
+    run(reader, max_value, false)
+}
+
+fn run<T>(reader: T, max_value: u32, part2: bool) -> Result<usize>
+where
+    T: BufRead,
+{
+    let line_re = Regex::new(r"^(\d+)-(\d+)$")?;
+    let mut blah = BTreeMap::new();
+
+    for line in valid_lines(reader) {
+        for caps in line_re.captures_iter(&line) {
+            let from = get_cap_x::<u32>(1, &caps)?;
+            let to = get_cap_x::<u32>(2, &caps)?;
+            let _ = blah.insert(from, to);
+        }
+    }
+
+    let mut max = 0;
+    let mut allowed = 0;
+    for (from, to) in blah {
+        if from <= max || from - 1 == max {
+            if to > max {
+                max = to;
+            }
+        } else {
+            if part2 {
+                allowed += from - max - 1;
+                if to > max {
+                    max = to;
+                }
+            } else {
+                break;
+            }
+        }
+    }
+
+    if part2 {
+        allowed += max_value - max;
+        Ok(usize::try_from(allowed)?)
+    } else {
+        Ok(usize::try_from(max + 1)?)
+    }
 }
 
 /// Solution for Part 2
@@ -51,15 +125,14 @@ pub fn part_2() -> Result<u32> {
 }
 
 fn find2(reader: BufReader<File>) -> usize {
-    find2_br(reader)
+    find2_br(reader, u32::MAX).map_err(print_err).unwrap_or_default()
 }
 
-fn find2_br<T>(reader: T) -> usize
+fn find2_br<T>(reader: T, max_value: u32) -> Result<usize>
 where
     T: BufRead,
 {
-    for _line in valid_lines(reader) {}
-    0
+    run(reader, max_value, true)
 }
 
 #[cfg(test)]
@@ -68,28 +141,30 @@ mod one_star {
     use anyhow::Result;
     use std::io::Cursor;
 
-    const TEST_1: &str = r">";
+    const TEST_1: &str = r"5-8
+0-2
+4-7";
 
     #[test]
     fn solution() -> Result<()> {
-        assert_eq!(find_br(Cursor::new(TEST_1)), 0);
+        assert_eq!(find_br(Cursor::new(TEST_1), 9)?, 3);
         Ok(())
     }
 }
 
 #[cfg(test)]
 mod two_star {
-    // use super::find2_br;
+    use super::find2_br;
     use anyhow::Result;
-    // use std::io::Cursor;
+    use std::io::Cursor;
 
-    // const TEST_1: &str = r"^v";
-    // const TEST_2: &str = r"^>v<";
-    // const TEST_3: &str = r"^v^v^v^v^v";
+    const TEST_1: &str = r"5-8
+0-2
+4-7";
 
     #[test]
     fn solution() -> Result<()> {
-        // assert_eq!(find2_br(Cursor::new(TEST_1))?, 3);
+        assert_eq!(find2_br(Cursor::new(TEST_1), 9)?, 2);
         Ok(())
     }
 }
