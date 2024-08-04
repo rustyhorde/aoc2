2a20148fdb6d24ba541f31388f1453eaa70600aa
Jason Ozias <jason.g.ozias@gmail.com>
2024-08-04 17:13:11 -0400

    AoC 2019 Day 1 and Day 2

--- aoc2-sol/src/year2019/day01.rs
+++ aoc2-sol/src/year2019/day01.rs
@@ -7,6 +7,43 @@
 // modified, or distributed except according to those terms.
 
 //! Advent of Code - Day 1
+//!
+//! --- Day 1: The Tyranny of the Rocket Equation ---
+//!
+//! Santa has become stranded at the edge of the Solar System while delivering presents to other planets! To accurately calculate his position in space, safely align his warp drive, and return to Earth in time to save Christmas, he needs you to bring him measurements from fifty stars.
+//!
+//! Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
+//!
+//! The Elves quickly load you into a spacecraft and prepare to launch.
+//!
+//! At the first Go / No Go poll, every Elf is Go until the Fuel Counter-Upper. They haven't determined the amount of fuel required yet.
+//!
+//! Fuel required to launch a given module is based on its mass. Specifically, to find the fuel required for a module, take its mass, divide by three, round down, and subtract 2.
+//!
+//! For example:
+//!
+//! - For a mass of 12, divide by 3 and round down to get 4, then subtract 2 to get 2.
+//! - For a mass of 14, dividing by 3 and rounding down still yields 4, so the fuel required is also 2.
+//! - For a mass of 1969, the fuel required is 654.
+//! - For a mass of 100756, the fuel required is 33583.
+//!
+//! The Fuel Counter-Upper needs to know the total fuel requirement. To find it, individually calculate the fuel needed for the mass of each module (your puzzle input), then add together all the fuel values.
+//!
+//! What is the sum of the fuel requirements for all of the modules on your spacecraft?
+//!
+//! --- Part Two ---
+//!
+//! During the second Go / No Go poll, the Elf in charge of the Rocket Equation Double-Checker stops the launch sequence. Apparently, you forgot to include additional fuel for the fuel you just added.
+//!
+//! Fuel itself requires fuel just like a module - take its mass, divide by three, round down, and subtract 2. However, that fuel also requires fuel, and that fuel requires fuel, and so on. Any mass that would require negative fuel should instead be treated as if it requires zero fuel; the remaining mass, if any, is instead handled by wishing really hard, which has no mass and is outside the scope of this calculation.
+//!
+//! So, for each module mass, calculate its fuel and add it to the total. Then, treat the fuel amount you just calculated as the input mass and repeat the process, continuing until a fuel requirement is zero or negative. For example:
+//!
+//! - A module of mass 14 requires 2 fuel. This fuel requires no further fuel (2 divided by 3 and rounded down is 0, which would call for a negative fuel), so the total fuel required is still just 2.
+//! - At first, a module of mass 1969 requires 654 fuel. Then, this fuel requires 216 more fuel (654 / 3 - 2). 216 then requires 70 more fuel, which requires 21 fuel, which requires 5 fuel, which requires no further fuel. So, the total fuel required for a module of mass 1969 is 654 + 216 + 70 + 21 + 5 = 966.
+//! - The fuel required by a module of mass 100756 and its fuel is: 33583 + 11192 + 3728 + 1240 + 411 + 135 + 43 + 12 + 2 = 50346.
+//!
+//! What is the sum of the fuel requirements for all of the modules on your spacecraft when also taking into account the mass of the added fuel? (Calculate the fuel requirements for each module separately, then add them all up at the end.)
 
 use crate::{
     constants::{AoCDay, AoCYear},
@@ -25,19 +62,28 @@ use std::{
 ///   [`AoCDay`](crate::constants::AoCDay) cannot be read.
 /// * This function will error if the elapsed [`std::time::Duration`] is invalid.
 pub fn part_1() -> Result<u32> {
-    run_solution::<usize>(AoCYear::AOC2019, AoCDay::AOCD01, find).map(|_| 0)
+    run_solution::<u32>(AoCYear::AOC2019, AoCDay::AOCD01, find).map(|_| 0)
 }
 
-fn find(reader: BufReader<File>) -> usize {
+fn find(reader: BufReader<File>) -> u32 {
     find_br(reader)
+        .map_err(|e| {
+            eprintln!("{e}");
+            e
+        })
+        .unwrap_or_default()
 }
 
-fn find_br<T>(reader: T) -> usize
+fn find_br<T>(reader: T) -> Result<u32>
 where
     T: BufRead,
 {
-    for _line in valid_lines(reader) {}
-    0
+    let mut digits = vec![];
+
+    for line in valid_lines(reader) {
+        digits.push(line.parse::<u32>()?);
+    }
+    Ok(digits.into_iter().map(|x| (x / 3) - 2).sum())
 }
 
 /// Solution for Part 2
@@ -47,45 +93,78 @@ where
 ///   [`AoCDay`](crate::constants::AoCDay) cannot be read.
 /// * This function will error if the elapsed [`std::time::Duration`] is invalid.
 pub fn part_2() -> Result<u32> {
-    run_solution::<usize>(AoCYear::AOC2019, AoCDay::AOCD01, find2).map(|_| 0)
+    run_solution::<u32>(AoCYear::AOC2019, AoCDay::AOCD01, find2).map(|_| 0)
 }
 
-fn find2(reader: BufReader<File>) -> usize {
+fn find2(reader: BufReader<File>) -> u32 {
     find2_br(reader)
+        .map_err(|e| {
+            eprintln!("{e}");
+            e
+        })
+        .unwrap_or_default()
 }
 
-fn find2_br<T>(reader: T) -> usize
+fn find2_br<T>(reader: T) -> Result<u32>
 where
     T: BufRead,
 {
-    for _line in valid_lines(reader) {}
-    0
+    let mut digits = vec![];
+
+    for line in valid_lines(reader) {
+        digits.push(line.parse::<u32>()?);
+    }
+    Ok(digits.into_iter().map(acc).sum())
+}
+
+fn acc(x: u32) -> u32 {
+    let fuel_by_three = x / 3;
+    if fuel_by_three < 2 {
+        0
+    } else {
+        let next = fuel_by_three - 2;
+        next + acc(next)
+    }
 }
 
 #[cfg(test)]
 mod one_star {
     use super::find_br;
+    use anyhow::Result;
     use std::io::Cursor;
 
-    const TEST_1: &str = r">";
+    const TEST_1: &str = r"12";
+    const TEST_2: &str = r"14";
+    const TEST_3: &str = r"1969";
+    const TEST_4: &str = r"100756";
 
     #[test]
-    fn solution() {
-        assert_eq!(find_br(Cursor::new(TEST_1)), 0);
+    fn solution() -> Result<()> {
+        assert_eq!(find_br(Cursor::new(TEST_1))?, 2);
+        assert_eq!(find_br(Cursor::new(TEST_2))?, 2);
+        assert_eq!(find_br(Cursor::new(TEST_3))?, 654);
+        assert_eq!(find_br(Cursor::new(TEST_4))?, 33583);
+        Ok(())
     }
 }
 
 #[cfg(test)]
 mod two_star {
-    // use super::find2_br;
-    // use std::io::Cursor;
+    use super::find2_br;
+    use anyhow::Result;
+    use std::io::Cursor;
 
-    // const TEST_1: &str = r"^v";
-    // const TEST_2: &str = r"^>v<";
-    // const TEST_3: &str = r"^v^v^v^v^v";
+    const TEST_1: &str = r"12";
+    const TEST_2: &str = r"14";
+    const TEST_3: &str = r"1969";
+    const TEST_4: &str = r"100756";
 
     #[test]
-    fn solution() {
-        // assert_eq!(find2_br(Cursor::new(TEST_1))?, 3);
+    fn solution() -> Result<()> {
+        assert_eq!(find2_br(Cursor::new(TEST_1))?, 2);
+        assert_eq!(find2_br(Cursor::new(TEST_2))?, 2);
+        assert_eq!(find2_br(Cursor::new(TEST_3))?, 966);
+        assert_eq!(find2_br(Cursor::new(TEST_4))?, 50346);
+        Ok(())
     }
 }
--- aoc2-sol/src/year2019/day02.rs
+++ aoc2-sol/src/year2019/day02.rs
@@ -7,12 +7,84 @@
 // modified, or distributed except according to those terms.
 
 //! Advent of Code - Day 2
+//! --- Day 2: 1202 Program Alarm ---
+//!
+//! On the way to your gravity assist around the Moon, your ship computer beeps angrily about a "1202 program alarm". On the radio, an Elf is already explaining how to handle the situation: "Don't worry, that's perfectly norma--" The ship computer bursts into flames.
+//!
+//! You notify the Elves that the computer's magic smoke seems to have escaped. "That computer ran Intcode programs like the gravity assist program it was working on; surely there are enough spare parts up there to build a new Intcode computer!"
+//!
+//! An Intcode program is a list of integers separated by commas (like 1,0,0,3,99). To run one, start by looking at the first integer (called position 0). Here, you will find an opcode - either 1, 2, or 99. The opcode indicates what to do; for example, 99 means that the program is finished and should immediately halt. Encountering an unknown opcode means something went wrong.
+//!
+//! Opcode 1 adds together numbers read from two positions and stores the result in a third position. The three integers immediately after the opcode tell you these three positions - the first two indicate the positions from which you should read the input values, and the third indicates the position at which the output should be stored.
+//!
+//! For example, if your Intcode computer encounters 1,10,20,30, it should read the values at positions 10 and 20, add those values, and then overwrite the value at position 30 with their sum.
+//!
+//! Opcode 2 works exactly like opcode 1, except it multiplies the two inputs instead of adding them. Again, the three integers after the opcode indicate where the inputs and outputs are, not their values.
+//!
+//! Once you're done processing an opcode, move to the next one by stepping forward 4 positions.
+//!
+//! For example, suppose you have the following program:
+//!
+//! `1,9,10,3,2,3,11,0,99,30,40,50`
+//!
+//! For the purposes of illustration, here is the same program split into multiple lines:
+//!
+//! > 1,9,10,3,
+//! > 2,3,11,0,
+//! > 99,
+//! > 30,40,50
+//!
+//! The first four integers, 1,9,10,3, are at positions 0, 1, 2, and 3. Together, they represent the first opcode (1, addition), the positions of the two inputs (9 and 10), and the position of the output (3). To handle this opcode, you first need to get the values at the input positions: position 9 contains 30, and position 10 contains 40. Add these numbers together to get 70. Then, store this value at the output position; here, the output position (3) is at position 3, so it overwrites itself. Afterward, the program looks like this:
+//!
+//! > 1,9,10,70,
+//! > 2,3,11,0,
+//! > 99,
+//! > 30,40,50
+//!
+//! Step forward 4 positions to reach the next opcode, 2. This opcode works just like the previous, but it multiplies instead of adding. The inputs are at positions 3 and 11; these positions contain 70 and 50 respectively. Multiplying these produces 3500; this is stored at position 0:
+//!
+//! > 3500,9,10,70,
+//! > 2,3,11,0,
+//! > 99,
+//! > 30,40,50
+//!
+//! Stepping forward 4 more positions arrives at opcode 99, halting the program.
+//!
+//! Here are the initial and final states of a few more small programs:
+//!
+//! > 1,0,0,0,99 becomes 2,0,0,0,99 (1 + 1 = 2).
+//! > 2,3,0,3,99 becomes 2,3,0,6,99 (3 * 2 = 6).
+//! > 2,4,4,5,99,0 becomes 2,4,4,5,99,9801 (99 * 99 = 9801).
+//! > 1,1,1,4,99,5,6,0,99 becomes 30,1,1,4,2,5,6,0,99.
+//!
+//! Once you have a working computer, the first step is to restore the gravity assist program (your puzzle input) to the "1202 program alarm" state it had just before the last computer caught fire. To do this, before running the program, replace position 1 with the value 12 and replace position 2 with the value 2. What value is left at position 0 after the program halts?
+//!
+//! --- Part Two ---
+//!
+//! "Good, the new computer seems to be working correctly! Keep it nearby during this mission - you'll probably use it again. Real Intcode computers support many more features than your new one, but we'll let you know what they are as you need them."
+//!
+//! "However, your current priority should be to complete your gravity assist around the Moon. For this mission to succeed, we should settle on some terminology for the parts you've already built."
+//!
+//! Intcode programs are given as a list of integers; these values are used as the initial state for the computer's memory. When you run an Intcode program, make sure to start by initializing memory to the program's values. A position in memory is called an address (for example, the first value in memory is at "address 0").
+//!
+//! Opcodes (like 1, 2, or 99) mark the beginning of an instruction. The values used immediately after an opcode, if any, are called the instruction's parameters. For example, in the instruction 1,2,3,4, 1 is the opcode; 2, 3, and 4 are the parameters. The instruction 99 contains only an opcode and has no parameters.
+//!
+//! The address of the current instruction is called the instruction pointer; it starts at 0. After an instruction finishes, the instruction pointer increases by the number of values in the instruction; until you add more instructions to the computer, this is always 4 (1 opcode + 3 parameters) for the add and multiply instructions. (The halt instruction would increase the instruction pointer by 1, but it halts the program instead.)
+//!
+//! "With terminology out of the way, we're ready to proceed. To complete the gravity assist, you need to determine what pair of inputs produces the output 19690720."
+//!
+//! The inputs should still be provided to the program by replacing the values at addresses 1 and 2, just like before. In this program, the value placed in address 1 is called the noun, and the value placed in address 2 is called the verb. Each of the two input values will be between 0 and 99, inclusive.
+//!
+//! Once the program has halted, its output is available at address 0, also just like before. Each time you try a pair of inputs, make sure you first reset the computer's memory to the values in the program (your puzzle input) - in other words, don't reuse memory from a previous attempt.
+//!
+//! Find the input noun and verb that cause the program to produce the output 19690720. What is 100 * noun + verb? (For example, if noun=12 and verb=2, the answer would be 1202.)
 
 use crate::{
     constants::{AoCDay, AoCYear},
     utils::{run_solution, valid_lines},
 };
 use anyhow::Result;
+use itertools::Itertools;
 use std::{
     fs::File,
     io::{BufRead, BufReader},
@@ -29,15 +101,57 @@ pub fn part_1() -> Result<u32> {
 }
 
 fn find(reader: BufReader<File>) -> usize {
-    find_br(reader)
+    find_br(reader, true)
 }
 
-fn find_br<T>(reader: T) -> usize
+fn find_br<T>(reader: T, replace: bool) -> usize
 where
     T: BufRead,
 {
-    for _line in valid_lines(reader) {}
-    0
+    let mut result = 0;
+    for line in valid_lines(reader) {
+        let mut csv: Vec<usize> = line
+            .split(',')
+            .filter_map(|x| {
+                if let Ok(val) = x.parse::<usize>() {
+                    Some(val)
+                } else {
+                    None
+                }
+            })
+            .collect();
+
+        if replace {
+            csv[1] = 12;
+            csv[2] = 2;
+        }
+        println!("initial: {csv:?}");
+        let mut op_code_idx = 0;
+        let mut op_code = csv[op_code_idx];
+
+        while op_code != 99 {
+            let idx_1 = csv[op_code_idx + 1];
+            let idx_2 = csv[op_code_idx + 2];
+            let idx_3 = csv[op_code_idx + 3];
+
+            if op_code == 1 {
+                let addend_1 = csv[idx_1];
+                let addend_2 = csv[idx_2];
+                csv[idx_3] = addend_1 + addend_2;
+            } else if op_code == 2 {
+                let factor_1 = csv[idx_1];
+                let factor_2 = csv[idx_2];
+                csv[idx_3] = factor_1 * factor_2;
+            } else {
+                break;
+            }
+            op_code_idx += 4;
+            op_code = csv[op_code_idx];
+        }
+        println!("final: {csv:?}");
+        result = csv[0];
+    }
+    result
 }
 
 /// Solution for Part 2
@@ -58,8 +172,57 @@ fn find2_br<T>(reader: T) -> usize
 where
     T: BufRead,
 {
-    for _line in valid_lines(reader) {}
-    0
+    let mut result = 0;
+    for line in valid_lines(reader) {
+        let initial: Vec<usize> = line
+            .split(',')
+            .filter_map(|x| {
+                if let Ok(val) = x.parse::<usize>() {
+                    Some(val)
+                } else {
+                    None
+                }
+            })
+            .collect();
+
+        let it = (0..100).permutations(2);
+
+        for nv_vec in it {
+            let mut csv = initial.clone();
+            let noun = nv_vec[0];
+            let verb = nv_vec[1];
+            csv[1] = noun;
+            csv[2] = verb;
+            let mut op_code_idx = 0;
+            let mut op_code = csv[op_code_idx];
+
+            while op_code != 99 {
+                let idx_1 = csv[op_code_idx + 1];
+                let idx_2 = csv[op_code_idx + 2];
+                let idx_3 = csv[op_code_idx + 3];
+
+                if op_code == 1 {
+                    let addend_1 = csv[idx_1];
+                    let addend_2 = csv[idx_2];
+                    csv[idx_3] = addend_1 + addend_2;
+                } else if op_code == 2 {
+                    let factor_1 = csv[idx_1];
+                    let factor_2 = csv[idx_2];
+                    csv[idx_3] = factor_1 * factor_2;
+                } else {
+                    break;
+                }
+                op_code_idx += 4;
+                op_code = csv[op_code_idx];
+            }
+            let result_at_zero = csv[0];
+            if result_at_zero == 19_690_720 {
+                result = 100 * noun + verb;
+                break;
+            }
+        }
+    }
+    result
 }
 
 #[cfg(test)]
@@ -67,11 +230,19 @@ mod one_star {
     use super::find_br;
     use std::io::Cursor;
 
-    const TEST_1: &str = r">";
+    const TEST_1: &str = r"1,9,10,3,2,3,11,0,99,30,40,50";
+    const TEST_2: &str = r"1,0,0,0,99";
+    const TEST_3: &str = r"2,3,0,3,99";
+    const TEST_4: &str = r"2,4,4,5,99,0";
+    const TEST_5: &str = r"1,1,1,4,99,5,6,0,99";
 
     #[test]
     fn solution() {
-        assert_eq!(find_br(Cursor::new(TEST_1)), 0);
+        assert_eq!(find_br(Cursor::new(TEST_1), false), 3500);
+        assert_eq!(find_br(Cursor::new(TEST_2), false), 2);
+        assert_eq!(find_br(Cursor::new(TEST_3), false), 2);
+        assert_eq!(find_br(Cursor::new(TEST_4), false), 2);
+        assert_eq!(find_br(Cursor::new(TEST_5), false), 30);
     }
 }
 
