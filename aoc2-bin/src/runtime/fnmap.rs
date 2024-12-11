a6fbb0935e10282c17dc4d88f111169764a41e6a
Jason Ozias <jason.ozias@kroger.com>
2024-12-11 13:42:24 -0500

    add vals to fnmap

--- aoc2-bin/src/runtime/fnmap.rs
+++ aoc2-bin/src/runtime/fnmap.rs
@@ -38,6 +38,8 @@ lazy_static! {
         bench_ins!(bench_map, AoCYear::AOC2024, AoCDay::AOCD07, year2024, day07);
         bench_ins!(bench_map, AoCYear::AOC2024, AoCDay::AOCD08, year2024, day08);
         bench_ins!(bench_map, AoCYear::AOC2024, AoCDay::AOCD09, year2024, day09);
+        bench_ins!(bench_map, AoCYear::AOC2024, AoCDay::AOCD10, year2024, day10);
+        bench_ins!(bench_map, AoCYear::AOC2024, AoCDay::AOCD11, year2024, day11);
 
         bench_map
     };
