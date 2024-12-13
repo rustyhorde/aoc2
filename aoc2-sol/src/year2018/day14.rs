2e476e501e34b315635af3c5da7c0dc5ea68bd25
Jason Ozias <jason.g.ozias@gmail.com>
2024-12-13 13:38:18 -0500

    Added AoC 2018 Day 14 from aoc

--- aoc2-sol/src/year2018/day14.rs
+++ aoc2-sol/src/year2018/day14.rs
@@ -10,10 +10,11 @@
 
 use crate::{
     constants::{AoCDay, AoCYear},
-    utils::{run_solution, valid_lines},
+    utils::{run_bench_solution, run_setup_solution, valid_lines},
 };
 use anyhow::Result;
 use std::{
+    collections::VecDeque,
     fs::File,
     io::{BufRead, BufReader},
 };
@@ -25,19 +26,123 @@ use std::{
 ///   [`AoCDay`](crate::constants::AoCDay) cannot be read.
 /// * This function will error if the elapsed [`std::time::Duration`] is invalid.
 pub fn part_1() -> Result<u32> {
-    run_solution::<usize>(AoCYear::AOC2018, AoCDay::AOCD14, find).map(|_| 0)
+    run_setup_solution::<Vec<String>, String>(AoCYear::AOC2018, AoCDay::AOCD14, setup, find)
+        .map(|_| 0)
 }
 
-fn find(reader: BufReader<File>) -> usize {
-    find_br(reader)
+/// Benchmark handler for Solution to Part 1
+///
+/// # Errors
+///
+pub fn part_1_bench(bench: u16) -> Result<u32> {
+    run_bench_solution::<Vec<String>, String>(bench, AoCYear::AOC2018, AoCDay::AOCD14, setup, find)
+        .map(|_| 0)
 }
 
-fn find_br<T>(reader: T) -> usize
+fn setup(reader: BufReader<File>) -> Vec<String> {
+    setup_br(reader).unwrap_or_default()
+}
+
+#[allow(clippy::unnecessary_wraps)]
+fn setup_br<T>(reader: T) -> Result<Vec<String>>
 where
     T: BufRead,
 {
-    for _line in valid_lines(reader) {}
-    0
+    let mut data = vec![];
+    for line in valid_lines(reader) {
+        data.push(line);
+    }
+    Ok(data)
+}
+
+#[allow(clippy::needless_pass_by_value)]
+fn find(data: Vec<String>) -> String {
+    find_res(data, false).unwrap_or_default()
+}
+
+#[allow(clippy::unnecessary_wraps)]
+fn find_res(data: Vec<String>, second_star: bool) -> Result<String> {
+    let mut recipe_count = String::new();
+
+    for line in data {
+        recipe_count = line;
+    }
+
+    score_recipes(&recipe_count, second_star)
+}
+
+fn check_patt(recipes: &VecDeque<u8>, pattern: &str, len: usize) -> bool {
+    if recipes.len() >= len {
+        let rev_patt: String = pattern.chars().rev().collect();
+        let mut check_str = String::new();
+        for i in recipes.iter().rev().take(len) {
+            check_str.push_str(&i.to_string());
+        }
+
+        if rev_patt == check_str {
+            return true;
+        }
+    }
+    false
+}
+
+fn score_recipes(count: &str, second_star: bool) -> Result<String> {
+    let len = count.len();
+    let count_u = count.parse::<usize>()?;
+    let mut recipe_deque = VecDeque::<u8>::new();
+    recipe_deque.push_back(3);
+    recipe_deque.push_back(7);
+
+    let mut idx_e1 = 0;
+    let mut idx_e2 = 1;
+
+    loop {
+        if !second_star && recipe_deque.len() > count_u + 10 {
+            break;
+        }
+        let e1_r = recipe_deque[idx_e1];
+        let e2_r = recipe_deque[idx_e2];
+        let next = e1_r + e2_r;
+
+        if next > 9 {
+            let tens = next / 10;
+            let ones = next % 10;
+
+            recipe_deque.push_back(tens);
+            if second_star && check_patt(&recipe_deque, count, len) {
+                break;
+            }
+            recipe_deque.push_back(ones);
+            if second_star && check_patt(&recipe_deque, count, len) {
+                break;
+            }
+        } else {
+            recipe_deque.push_back(next);
+            if second_star && check_patt(&recipe_deque, count, len) {
+                break;
+            }
+        }
+
+        let len = recipe_deque.len();
+
+        idx_e1 = (idx_e1 + e1_r as usize + 1) % len;
+        idx_e2 = (idx_e2 + e2_r as usize + 1) % len;
+    }
+
+    let output = if second_star {
+        let total_len = recipe_deque.len();
+        let _res = recipe_deque.split_off(total_len - len);
+        recipe_deque.len().to_string()
+    } else {
+        let after_count = recipe_deque.split_off(count_u);
+        after_count
+            .into_iter()
+            .take(10)
+            .map(|x| x.to_string())
+            .collect()
+    };
+
+    Ok(output)
 }
 
 /// Solution for Part 2
@@ -47,45 +152,70 @@ where
 ///   [`AoCDay`](crate::constants::AoCDay) cannot be read.
 /// * This function will error if the elapsed [`std::time::Duration`] is invalid.
 pub fn part_2() -> Result<u32> {
-    run_solution::<usize>(AoCYear::AOC2018, AoCDay::AOCD14, find2).map(|_| 0)
+    run_setup_solution::<Vec<String>, String>(AoCYear::AOC2018, AoCDay::AOCD14, setup, find2)
+        .map(|_| 0)
 }
 
-fn find2(reader: BufReader<File>) -> usize {
-    find2_br(reader)
+/// Benchmark handler for Solution to Part 2
+///
+/// # Errors
+///
+pub fn part_2_bench(bench: u16) -> Result<u32> {
+    run_bench_solution::<Vec<String>, String>(bench, AoCYear::AOC2018, AoCDay::AOCD14, setup, find2)
+        .map(|_| 0)
 }
 
-fn find2_br<T>(reader: T) -> usize
-where
-    T: BufRead,
-{
-    for _line in valid_lines(reader) {}
-    0
+#[allow(clippy::needless_pass_by_value)]
+fn find2(data: Vec<String>) -> String {
+    find_res(data, true).unwrap_or_default()
 }
 
 #[cfg(test)]
 mod one_star {
-    use super::find_br;
+    use super::{find, setup_br};
+    use anyhow::Result;
     use std::io::Cursor;
 
-    const TEST_1: &str = r">";
+    const TEST_1: &str = r"5";
+    const TEST_2: &str = r"9";
+    const TEST_3: &str = r"18";
+    const TEST_4: &str = r"2018";
 
     #[test]
-    fn solution() {
-        assert_eq!(find_br(Cursor::new(TEST_1)), 0);
+    fn solution() -> Result<()> {
+        let data = setup_br(Cursor::new(TEST_1))?;
+        assert_eq!(find(data), "0124515891");
+        let data = setup_br(Cursor::new(TEST_2))?;
+        assert_eq!(find(data), "5158916779");
+        let data = setup_br(Cursor::new(TEST_3))?;
+        assert_eq!(find(data), "9251071085");
+        let data = setup_br(Cursor::new(TEST_4))?;
+        assert_eq!(find(data), "5941429882");
+        Ok(())
     }
 }
 
 #[cfg(test)]
 mod two_star {
-    // use super::find2_br;
-    // use std::io::Cursor;
+    use super::{find2, setup_br};
+    use anyhow::Result;
+    use std::io::Cursor;
 
-    // const TEST_1: &str = r"^v";
-    // const TEST_2: &str = r"^>v<";
-    // const TEST_3: &str = r"^v^v^v^v^v";
+    const TEST_1: &str = r"51589";
+    const TEST_2: &str = r"01245";
+    const TEST_3: &str = r"92510";
+    const TEST_4: &str = r"59414";
 
     #[test]
-    fn solution() {
-        // assert_eq!(find2_br(Cursor::new(TEST_1))?, 3);
+    fn solution() -> Result<()> {
+        let data = setup_br(Cursor::new(TEST_1))?;
+        assert_eq!(find2(data), "9");
+        let data = setup_br(Cursor::new(TEST_2))?;
+        assert_eq!(find2(data), "5");
+        let data = setup_br(Cursor::new(TEST_3))?;
+        assert_eq!(find2(data), "18");
+        let data = setup_br(Cursor::new(TEST_4))?;
+        assert_eq!(find2(data), "2018");
+        Ok(())
     }
 }
